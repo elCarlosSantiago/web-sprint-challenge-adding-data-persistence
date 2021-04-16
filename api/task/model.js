d7025657ff9e1a70024381149670fc5a8f790248
CarlosSantiago Nevett <csnevett@gmail.com>
2021-04-16 16:37:07 -0400

    all get methods up

--- api/task/model.js
+++ api/task/model.js
@@ -1,7 +1,17 @@
 const db = require('../../data/dbConfig.js');
 
 const getTasks = async () => {
-  return db('tasks');
+  const taskCollection = await db('tasks as t')
+    .join('projects as p', 't.project_id', 'p.project_id')
+    .select('t.*', 'p.project_name', 'p.project_description');
+
+  const formattedTask = taskCollection.map((task) => {
+    return {
+      ...task,
+      task_completed: task.task_completed == 1 ? true : false,
+    };
+  });
+  return formattedTask
 };
 
 const addTask = async (task) => {
