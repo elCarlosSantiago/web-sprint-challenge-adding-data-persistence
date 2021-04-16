7f8f8e7532b5d8ce8b1a1d7b8d39fa58c6803674
CarlosSantiago Nevett <csnevett@gmail.com>
2021-04-16 12:57:43 -0400

    server listening

--- api/server.js
+++ api/server.js
@@ -1,1 +1,20 @@
-// build your server here and require it from index.js
+const express = require('express');
+const helmet = require('helmet')
+// const ProjectRouter = require('./project/router')
+// const ResourceRouter = require('./resource/router')
+// const TaskRouter = require('./task/router')
+
+const server = express();
+
+server.use(express.json())
+server.use(helmet())
+// server.use('/api/projects', ProjectRouter)
+// server.use('/api/resources', ResourceRouter)
+// server.use('/api/tasks', TaskRouter)
+
+server.use('*', (req, res) => {
+  res.status(404).json('Resource not found')
+})
+
+module.exports = server;
+
--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+require('dotenv').config();
+const server = require('./api/server.js');
 
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  if (PORT === 5000) {
+    console.log(`SET UP ENV Listening on port ${PORT}...`);
+  } else {
+    console.log(`Listening on port ${PORT}`);
+  }
+});
